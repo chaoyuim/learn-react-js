ad87138e11f048b46e297e58d182a51ce24b53c9
Chao Yu <chao.yu@hotmail.com>
2022-02-07 22:32:23 +0100

    -- save file

---
+++ react-001/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Greeting = () => {
+  return (
+    <h3></h3>
+  )
+}
+
+
+
+ReactDOM.render(<Greeting />, document.getElementById('root'))
+
